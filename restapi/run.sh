7858c03404b25fcb56a1c9ab386f402e54692cf3
tmarchwicki <tymonn@wp.pl>
2024-09-25 22:27:33 +0200

    Update run.sh - removed nit needed lines

--- restapi/run.sh
+++ restapi/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#source ~/ENVS/jsonserver/bin/activate
 cd "$(dirname "$0")"
 uvicorn main_neo4j:app --reload --port 8000 --host 0.0.0.0
 # uvicorn main:app --reload --port 8000 --host 0.0.0.0
